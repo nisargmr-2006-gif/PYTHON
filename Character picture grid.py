757a4f60651d56e68bb8eebf0a655c27dbb1afc1
nisargmr-2006-gif <nisargl.24.beai@acharya.ac.in>
2025-05-27 16:39:43 +0530

    Create Character picture grid.py

---
+++ Character picture grid.py
@@ -0,0 +1,16 @@
+#charactergrid
+print("Nisarg ,USN:1AY24AI077,SEC:O")
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+for y in range(len(grid[0])):
+    for x in range(len(grid)):
+        print(grid[x][y], end='')
+    print()
