351b50fec241ee3c8e2d8d5e7e0d918289b984a0
nisargmr-2006-gif <nisargl.24.beai@acharya.ac.in>
2025-05-27 16:45:25 +0530

    Create Zigzag.py

---
+++ Zigzag.py
@@ -0,0 +1,10 @@
+#zigzag
+print("Nisarg ,USN:1AY24AI077,SEC:O")
+lines = 9
+max_spaces = lines 
+for i in range(lines):
+    if i <= max_spaces:
+        spaces = i
+    else:
+        spaces = lines - i - 1
+    print(" " * spaces + "*" * 8)
